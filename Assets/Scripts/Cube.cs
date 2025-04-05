ffff873c93119c5e89dee1362df71c510c4fff0c
MusabAlsadig <musabalsadig2@gmail.com>
2025-04-05 02:15:08 +0200

    #3 Encapsulation

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Cube : Shape
 {
+    public void SetForce(int force)
+    {
+        this.force = Mathf.Abs(force);
+    }
+
+    public int GetForce()
+    {
+        return force;
+    }
+
     protected override void Pushup()
     {
         rigidbody.AddForce(transform.up * force, ForceMode.Acceleration);
--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Sphere : Shape
 {
+    public void SetForce(int force)
+    {
+        this.force = Mathf.Abs(force);
+    }
+
+    public int GetForce()
+    {
+        return force;
+    }
+
     protected override void Pushup()
     {
         rigidbody.AddForce(transform.up * force, ForceMode.VelocityChange);
